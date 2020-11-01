3390e3a873818ec62a1a46173ce6ff311003365d
Javs <javs.doblerodriguez@gmail.com>
2020-11-01 18:11:02 +0100

    header

---
+++ constantes.h
@@ -0,0 +1,32 @@
+#ifndef __CONSTANTES_H
+#define __CONSTANTES_H
+
+#define MAIN 500
+#define BEGIN_BLOCK 501
+#define END_BLOCK 502
+#define SEMICOLON 503
+#define COMMA 504
+#define LEFT_PARENTHE 505
+#define RIGHT_PARENTHE 506
+#define ASSIGNMENT 507
+#define IF 508
+#define ELSE 509
+#define WHILE 510
+#define SWITCH 511
+#define CASE 5012
+#define RETURN 513
+#define ELEM_TYPE 514
+#define MODIFIER 515
+#define INPUT 516
+#define OUTPUT 517
+#define SIGN 518
+#define UNARY_OP 519
+#define BINARY_OP 520
+#define LOGIC_VAR 521
+#define INT_VAR 522
+#define REAL_VAR 523
+#define CHAR_VAR 524
+#define LISTING 525
+#define CHAIN 526
+
+#endif
